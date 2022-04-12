4cce8b2fe3a81e6e4b15deb0fbd131ea1240fc67
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-12 13:35:04 +0530

    Added: LinkedHashMap class and store node using arraylist

--- src/com/bl/hash_map/storage/test_case/MyHashMapTest.java
+++ src/com/bl/hash_map/storage/test_case/MyHashMapTest.java
@@ -2,6 +2,7 @@ package com.bl.hash_map.storage.test_case;
 
 import com.bl.hash_map.storage.MyHashMap;
 import com.bl.hash_map.storage.MyMapNode;
+import com.bl.linked_hash_map.storage.MyLinkedHashMap;
 import com.bl.linked_list.storage.INode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,22 @@ public class MyHashMapTest {
         int frequency = myHashMap.get("not");
         System.out.println(myHashMap.toString());
         Assertions.assertEquals(1, frequency);
+    }
+
+    //UC2 Linked hash map
+    @Test
+    public void giveASentence_ShouldCreateMapAndCheckFrequencyOfWordPassTheResult() {
+        String sentence = "Paranoids are not paranoid because they are paranoid " + "but because they keep putting themselves deliberately" + " into paranoid avoidable situations";
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null) value = 1;
+            else value = value + 1;
+            myLinkedHashMap.add(word, value);
+        }
+        int frequency = myLinkedHashMap.get("paranoid");
+        System.out.println(myLinkedHashMap);
+        Assertions.assertEquals(3, frequency);
     }
 }
---
+++ src/com/bl/linked_hash_map/storage/MyLinkedHashMap.java
@@ -0,0 +1,52 @@
+package com.bl.linked_hash_map.storage;
+
+import com.bl.hash_map.storage.MyMapNode;
+import com.bl.linked_list.storage.MyLinkedList;
+
+import java.util.ArrayList;
+
+public class MyLinkedHashMap<K, V> {
+    private final int noOfBucket;
+    private final ArrayList<MyLinkedList<K>> myBucketArray;
+
+    public MyLinkedHashMap() {
+        this.noOfBucket = 10;
+        this.myBucketArray = new ArrayList<>(noOfBucket);
+        for (int i = 0; i < noOfBucket; i++) {
+            this.myBucketArray.add(null);
+        }
+    }
+
+    public int getBucketIndex(K key) {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % noOfBucket;
+        return index;
+    }
+
+    public V get(K key) {
+        int index = getBucketIndex(key);
+        MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+        if (myLinkedList == null) return null;
+        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+        return (myMapNode == null) ? null : myMapNode.getValue();
+    }
+
+    public void add(K key, V value) {
+        int index = getBucketIndex(key);
+        MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+        if (myLinkedList == null) {
+            myLinkedList = new MyLinkedList<>();
+            this.myBucketArray.set(index, myLinkedList);
+        }
+        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+        if (myMapNode == null) {
+            myMapNode = new MyMapNode<>(key, value);
+            myLinkedList.append(myMapNode);
+        } else myMapNode.setValue(value);
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedHashMapNodes{ " + myBucketArray + " }";
+    }
+}
